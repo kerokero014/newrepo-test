22194e0266ae0739e5fff9faea208101fc2e4bd3
kerokero014 <kevinjrm.kr@gmail.com>
2023-09-18 16:54:53 -0600

    worked now

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set("layout", "./layouts/layout"); // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static);
+app.use(require("./routes/static"));
 //index route
 app.get("/", function(req, res) {
   res.render("index", {title: "Home"});
