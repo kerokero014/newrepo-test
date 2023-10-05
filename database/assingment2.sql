409b4d1c6aa4693d1db3e6e89f3abb8d73640aad
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-04 20:39:37 -0600

    finsihed....

--- database/assingment2.sql
+++ database/assingment2.sql
@@ -28,3 +28,7 @@ FROM inventory
 INNER JOIN classification ON inventory.classification_id = classification.classification_id
 WHERE classification.classification_name = 'Sport';
 
+UPDATE public.inventory
+SET 
+	inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
