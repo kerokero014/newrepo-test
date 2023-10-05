1710122bc0f3fb7023908a6ee0a3b5a013734192
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-05 15:27:48 -0600

    updated

--- database/sql-db.sql
+++ database/sql-db.sql
@@ -49,9 +49,14 @@ CREATE TABLE IF NOT EXISTS public.account
 );
 
 INSERT INTO public.classification (classification_name)
-VALUES ('Custom'),
+VALUES
+	('Custom'),
 	('Sport'),
 	('SUV'),
 	('Truck'),
 	('Sedan');
 	
+UPDATE public.inventory
+SET 
+	inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
