a08d18cf563a1c4f348d5d939c38edf33a315b5b
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-04 20:19:16 -0600

    PROGRESS...

--- database/assingment2.sql
+++ database/assingment2.sql
@@ -23,4 +23,8 @@ UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'huge interior')
 WHERE inv_make = 'GM';
 
-.
+SELECT inventory.inv_make, inventory.inv_model, classification.classification_name AS classification_name
+FROM inventory
+INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
+
