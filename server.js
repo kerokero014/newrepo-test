32b41d39f849e18175946341fe42e3541b204bf3
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-12 11:13:25 -0600

    route, controller, and model

--- server.js
+++ server.js
@@ -25,9 +25,14 @@ app.set("layout", "./layouts/layout"); // not at views root
 app.use(static);
 //index route
 app.get("/", baseController.buildHome)
+
 //app.get("/", function(req, res) {
 //  res.render("index", {title: "Home"});
 //});
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 
 /* ***********************
  * Local Server Information
