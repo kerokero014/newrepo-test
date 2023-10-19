37246e1c3f7cff400aba55f4737d8b041a3e5452
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-19 14:03:20 -0600

    erros in footer

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,13 +1,14 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
-const invController = require("../controllers/invController")
+const invController = require("../controllers/invController");
+const { handleErrors } = require("../utilities");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", handleErrors(invController.buildByClassificationId));
 // #3
 // Route to build inventory by vehicle view
-router.get("/detail/:invId", invController.buildByInvId);
+router.get("/detail/:invId", handleErrors(invController.buildByInvId));
 
 
 
