794dcdef3c7ae4ad9eedebc03a2eea9930f61b3b
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-23 18:36:05 -0600

    first prep task done

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -6,6 +6,11 @@ baseController.buildHome = async function(req, res){
   res.render("index", {title: "Home", nav})
 }
 
+baseController.buildHome = async function (req, res){
+  const nav = await utilities.getNav()
+  req.flash("notice", "This is a flash message.")
+  res.render("index", {title: "Home", nav})
+}
 
 
 module.exports = baseController
