16529e7bbca7dbcddde4ab15696950621ef5ef52
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-23 22:10:23 -0600

    updates but not all the way there

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,12 +5,13 @@ const utilities = require('../utilities')
 /* ****************************************
 *  Deliver login view
 * *************************************** */
+
 async function buildLogin(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/login", {
-      title: "Login",
-      nav,
-    })
-  }
+  let nav = await utilities.getNav()
+  res.render("account/log-in", {
+    title: "Login",
+    nav,
+  })
+}
   
-  module.exports = { buildLogin }
+module.exports = { buildLogin }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,15 +7,8 @@ const utilities = require("../utilities")
 /* ****************************************
 *  Deliver login view
 * *************************************** */
-router.get("/login", utilities.handleErrors(accountController.buildLogin))
+router.get("/log-in", utilities.handleErrors(accountController.buildLogin))
 
 
-async function buildLogin(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/login", {
-      title: "Login",
-      nav,
-    })
-  }
-  
-  module.exports = { buildLogin }
+
+module.exports = router
