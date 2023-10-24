9df190d22cb72b48e272e37643b5696180831f74
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-23 21:39:32 -0600

    login stuff

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,6 +7,9 @@ const utilities = require("../utilities")
 /* ****************************************
 *  Deliver login view
 * *************************************** */
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+
 async function buildLogin(req, res, next) {
     let nav = await utilities.getNav()
     res.render("account/login", {
