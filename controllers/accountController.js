f5a24444cabc7879379eff26f06d64fc11b9cc00
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-31 14:55:54 -0600

    nothing not working

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -61,4 +61,4 @@ async function registerAccount(req, res) {
   }
 }
 
-module.exports = { buildLogin, buildRegister };
+module.exports = { buildLogin, buildRegister, registerAccount };
--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,14 @@
-const pswdBtn = document.querySelector("#pswdBtn");
-pswdBtn.addEventListener("click", function() {
-  const pswdInput = document.getElementById("pword");
-  const type = pswdInput.getAttribute("type");
-  if (type == "password") {
-    pswdInput.setAttribute("type", "text");
-    pswdBtn.innerHTML = "Hide Password";
-  } else {
-    pswdInput.setAttribute("type", "password");
-    pswdBtn.innerHTML = "Show Password";
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  const pswdBtn = document.getElementById("pswdBtn");
+  pswdBtn.addEventListener("click", function() {
+    const pswdInput = document.getElementById("accountPassword_pword");
+    const type = pswdInput.getAttribute("type");
+    if (type === "password") {
+      pswdInput.setAttribute("type", "text");
+      pswdBtn.textContent = "Hide Password";
+    } else {
+      pswdInput.setAttribute("type", "password");
+      pswdBtn.textContent = "Show Password";
+    }
+  });
 });
