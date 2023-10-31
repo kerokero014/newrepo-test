644c630a10dfb3b647246a44a99e1acb63f748fc
kerokero014 <kevinjrm.kr@gmail.com>
2023-10-30 21:54:13 -0600

    progress

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,21 +1,26 @@
-const express = require("express")
-const router = new express.Router()
-const accountController = require("../controllers/accountController")
-const utilities = require("../utilities")
-
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
+const regValidate = require("../utilities/account-validation");
 
 /* ****************************************
-*  Deliver login view
-* *************************************** */
-router.get("/log-in", utilities.handleErrors(accountController.buildLogin))
+ *  Deliver login view
+ * *************************************** */
+router.get("/log-in", utilities.handleErrors(accountController.buildLogin));
 
-// Resgitration route 
-router.get("/register", utilities.handleErrors(accountController.buildRegister))
+// Resgitration route
+router.get(
+    "/register",
+    utilities.handleErrors(accountController.buildRegister)
+);
 
+// Process the registration data
 router.post(
-    "/regiter",
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
     utilities.handleErrors(accountController.registerAccount)
 )
 
-
-module.exports = router
+module.exports = router;
