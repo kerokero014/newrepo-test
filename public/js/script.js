2c2aa9d566bf57ded2464d77429e514d89323b09
kerokero014 <kevinjrm.kr@gmail.com>
2023-11-03 01:24:23 -0600

    not working still

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const pswdBtn = document.getElementById("pswdBtn");
-  pswdBtn.addEventListener("click", function() {
-    const pswdInput = document.getElementById("accountPassword_pword");
-    const type = pswdInput.getAttribute("type");
-    if (type === "password") {
-      pswdInput.setAttribute("type", "text");
-      pswdBtn.textContent = "Hide Password";
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the password input element
+  var passwordInput = document.getElementById('accountPassword pword');
+  // Get the "Show password" button
+  var showPasswordButton = document.getElementById('pswdBtn').getElementsByTagName('button')[0];
+
+  // Add click event listener to the button
+  showPasswordButton.addEventListener('click', function () {
+    // Check the type of the password input
+    if (passwordInput.type === 'password') {
+      // If it's currently a password field, change it to text
+      passwordInput.type = 'text';
+      // Change the button text to 'Hide password'
+      showPasswordButton.textContent = 'Hide password';
     } else {
-      pswdInput.setAttribute("type", "password");
-      pswdBtn.textContent = "Show Password";
+      // If it's currently a text field, change it to password
+      passwordInput.type = 'password';
+      // Change the button text to 'Show password'
+      showPasswordButton.textContent = 'Show password';
     }
   });
 });
