3e889d6ba248ee56d59d7af030dd34682599fa22
kerokero014 <kevinjrm.kr@gmail.com>
2023-11-06 14:39:00 -0700

    nothings yet

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities");
-
 const regValidate = require("../utilities/account-validation");
 
 // Route for login
