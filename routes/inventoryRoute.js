52f8abaa6095e87dd74804f4060adf725c174a51
kerokero014 <kevinjrm.kr@gmail.com>
2023-11-06 14:51:16 -0700

    nothing

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,16 +1,17 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
+const utilities = require("../utilities");
 const invController = require("../controllers/invController");
 const { handleErrors } = require("../utilities");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", handleErrors(invController.buildByClassificationId));
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 // #3
 // Route to build inventory by vehicle view
-router.get("/detail/:invId", handleErrors(invController.buildByInvId));
+router.get("/detail/:invId", utilities.handleErrors(invController.buildByInvId));
 
 //#
-router.get("/broken", handleErrors(invController.buildBrokenPage));
+router.get("/broken", utilities.handleErrors(invController.buildBrokenPage));
 
 module.exports = router;
