97e24cbff4195d28aefea54764a5661a6f8db0f5
kerokero014 <kevinjrm.kr@gmail.com>
2023-11-06 20:54:34 -0700

    nothing

--- models/account-model.js
+++ models/account-model.js
@@ -1,8 +1,8 @@
 const pool = require("../database/");
 
 /* *****************************
- *   Register new account
- * *************************** */
+*   Register new account
+* *************************** */
 async function registerAccount(account_firstname, account_lastname, account_email, account_password){
   try {
     const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *"
