d5c668f6f6551126dd56c33eabf24455255803a8
kerokero014 <kevinjrm.kr@gmail.com>
2023-11-14 22:15:36 -0700

    Add test accounts and display messages and errors in account.ejs

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -63,4 +63,25 @@ validate.checkRegData = async (req, res, next) => {
   next();
 };
 
+
+////TEST ACCOUNTS 
+/*Basic */
+//account_firstname: Basic
+//account_lastname: Client
+//account_email: basic@340.edu
+//account_password: I@mABas1cCl!3nt
+
+/**Employee */
+//account_firstname: Happy
+//account_lastname: Employee
+//account_email: happy@340.edu
+//account_password: I@mAnEmpl0y33
+
+/**Adming */
+//account_firstname: Manager
+//account_lastname: User
+//account_email: manager@340.edu
+//account_password: I@mAnAdm!n1strat0r
+
+
 module.exports = validate;
