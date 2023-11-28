c05458fc15841f1d3bd62b78ad4f35624a8480ae
kerokero014 <kevinjrm.kr@gmail.com>
2023-11-28 15:33:51 -0700

    Refactor email validation and check for existing email

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -123,12 +123,28 @@ validate.updateAccountRules = () => {
       .isLength({ min: 2 })
       .withMessage("Please provide a last name."), // on error this message is sent.
 
-    // valid email is required and cannot already exist in the DB
+    // valid email is required
+    // if email is being changed, it cannot already exist in the database
     body("account_email")
-      .trim()
-      .isEmail()
-      .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+    .trim()
+    .isEmail()
+    .normalizeEmail() // refer to validator.js docs
+    .withMessage("A valid email is required.")
+    .custom(async (account_email, { req }) => {
+    const account_id = req.body.account_id
+    const account = await accountModel.getAccountById(account_id)
+    // Check if submitted email is same as existing
+    if (account_email != account.account_email) {
+    // No - Check if email exists in table
+    const emailExists = await accountModel.checkExistingEmail(
+    account_email
+    )
+    // Yes - throw error
+    if (emailExists.count != 0) {
+    throw new Error("Email exists. Please use a different email")
+    }
+    }
+    }),
   ]
 }
 
